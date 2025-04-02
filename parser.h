ae2abb7761b4bcdb3a46ecb09b2f20da34e3a9ba
Cherry-Trees <147207363+Cherry-Trees@users.noreply.github.com>
2025-04-02 17:58:14 -0500

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,11 +1,24 @@
+/**
+ *  Author: Jamie Miles
+ *  Date:   4/1/2025
+ * 
+ *  Parser header file.
+ *  Defines functions representing nonterminals in the grammar.
+ */
+
 #ifndef PARSER_H
 #define PARSER_H
 
 #include "lexer.h"
+#include "memory.h"
 
-void prog(Token *token, const char *is, size_t *index);
-void slist(Token *token, const char *is, size_t *index);
-void stmt(Token *token, const char *is, size_t *index);
-void rhs(Token *token, const char *is, size_t *index);
+// Nonterminals.
+void prog(const char *is, int size);
+void slist(Token *token, const char *is, int *index, Node *freeList, Node *symbolTable[HASH_TABLE_SIZE]);
+Node *stmt(Token *token, const char *is, int *index, Node *freeList, Node *symbolTable[HASH_TABLE_SIZE]);
+Node *rhs(Token *token, const char *is, int *index, Node *freeList, Node *symbolTable[HASH_TABLE_SIZE], char nameLhs[ID_MAX_SIZE]);
+
+// Throw error.
+void error(const char *msg, const int lineno, Node *freeList, Node *symbolTable[HASH_TABLE_SIZE]);
 
 #endif

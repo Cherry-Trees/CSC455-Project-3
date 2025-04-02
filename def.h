0e774b71aac37026e737676a9099295041a583c0
Cherry-Trees <147207363+Cherry-Trees@users.noreply.github.com>
2025-04-02 17:55:33 -0500

    Create def.h

---
+++ def.h
@@ -0,0 +1,16 @@
+#ifndef DEF_H
+#define DEF_H
+
+#define NUM_STATES 7
+#define ID_MAX_SIZE 64
+#define HASH_TABLE_SIZE 64
+#define FNAME_MAX_SIZE 128
+#define RBUF_SIZE 128
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#endif

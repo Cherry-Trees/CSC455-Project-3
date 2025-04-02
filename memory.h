8d017b9ad02fce98e832c5267914b116da2b99f8
Cherry-Trees <147207363+Cherry-Trees@users.noreply.github.com>
2025-04-02 17:57:41 -0500

    Update memory.h

--- memory.h
+++ memory.h
@@ -1,23 +1,30 @@
+/**
+ *  Author: Jamie Miles
+ *  Date:   4/1/2025 
+ * 
+ *  Memory header file.
+ *  Defines structs and functions necessary for memory management.
+ */
+
 #ifndef MEMORY_H
 #define MEMORY_H
 
-#include "global.h"
+#include "def.h"
 
+// Structure that holds starting address, size, and reference count.
 typedef struct {
-    size_t addr, size, refs;
+    int addr, size, refs;
 } Block;
 
+// Structure that holds a string and block pointer.
 typedef struct {
-    char name[ID_MAX_SIZE];
-    Block *block;
+    char name[ID_MAX_SIZE]; // Name of variable.
+    Block *block;           // Block of memory the variable is referring to.
 } Variable;
 
+// Structure representing a singly list node that holds either a block or variable.
 typedef struct Node Node;
 struct Node {
-    // enum {
-    //     BLOCK,
-    //     VAR
-    // } tag;
     union {
         Block block;
         Variable var;
@@ -25,23 +32,21 @@ struct Node {
     Node *next;
 };
 
-
-
-
-
+// Memory management operations.
 Node *compress(Node *freeList);
 Node *associate(Node *freeList, Node *symbolTable[HASH_TABLE_SIZE], char nameLhs[ID_MAX_SIZE], char nameRhs[ID_MAX_SIZE]);
 void dump(Node *freeList, Node *symbolTable[HASH_TABLE_SIZE]);
-Node *sort(Node *freeList);
-Node *alloc(Node *freeList, Node *symbolTable[HASH_TABLE_SIZE], char name[ID_MAX_SIZE], const size_t size);
+Node *alloc(Node *freeList, Node *symbolTable[HASH_TABLE_SIZE], char name[ID_MAX_SIZE], const int size);
 Node *dealloc(Node *freeList, Node *symbolTable[HASH_TABLE_SIZE], char name[ID_MAX_SIZE]);
 
-size_t index(char name[ID_MAX_SIZE]);
-Variable *find(Node *symbolTable[HASH_TABLE_SIZE], char name[ID_MAX_SIZE]);
-void insert(Node *symbolTable[HASH_TABLE_SIZE], Variable var);
-void remove(Node *symbolTable[HASH_TABLE_SIZE], char name[ID_MAX_SIZE]);
-Node *push(Node *freeList, Block block);
-
-
+// Data structure operations.
+int indexName(char name[ID_MAX_SIZE]);
+Variable *findST(Node *symbolTable[HASH_TABLE_SIZE], char name[ID_MAX_SIZE]);
+void insertST(Node *symbolTable[HASH_TABLE_SIZE], Variable var);
+void removeST(Node *symbolTable[HASH_TABLE_SIZE], char name[ID_MAX_SIZE]);
+Node *sortFL(Node *freeList);
+Node *pushFL(Node *freeList, Block block);
+void clearST(Node *freeList, Node *symbolTable[HASH_TABLE_SIZE]);
+void clearFL(Node *freeList);
 
 #endif
